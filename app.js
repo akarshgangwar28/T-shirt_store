6b308a2d73c4b0123d3ae9752a98a2ce2716bee0
akarshgangwar28 <akarshprog28@gmail.com>
2021-12-10 19:17:22 +0530

    All set for login route and middleware(auth.js) is also good to go

--- app.js
+++ app.js
@@ -1,84 +1,100 @@
-const express=require("express")
+const express = require("express");
 require("dotenv").config();
 require("./config/database").connect();
-const jwt=require("jsonwebtoken")
-const User=require("./models/user")
-const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+const auth=require("./middleware/auth")
 
-const app=express();
- 
-app.get('/',(req,res)=> 
-{
-    res.send("<h1>Hello from auth system</h1>");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from auth system</h1>");
 });
 
-app.use(express.json());
+app.use(express.json()); // for parsing req.body (beacuse body is unparsed always)
 
-app.post("/register",async(req,res)=>{
+app.post("/register", async (req, res) => {
+  try {
+    const { firstname, lastname, email, password } = req.body;
 
-   try { 
-
-    const {firstname , lastname, email, password}=req.body;
-
-    if( !(email && firstname && lastname && password))
-    {
-        res.status(400).send("All fields are required")
-    }
- 
-    const existingUser= await User.findOne({email})
-
-    if(existingUser)                                    // check if user is not already registred or not
-    {   
-        res.status(401).send("User Already registered")
+    if (!(email && firstname && lastname && password)) {
+      res.status(400).send("All fields are required");
     }
 
-    const encPassword=await  bcrypt.hash(password,10);     //encyrpt the password
+    const existingUser = await User.findOne({ email });
 
-    const user=await User.create({                         // new user info saved into DB or (create a document in database)
-        firstname,
-        lastname,
-        email:email.toLowerCase(),
-        password:encPassword
-    })
+    if (existingUser) {
+      // check if user is not already registred or not
+      res.status(401).send("User Already registered");
+    }
+
+    const encPassword = await bcrypt.hash(password, 10); //encyrpt the password
+
+    const user = await User.create({
+      // new user info saved into DB or (create a document in database)
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      password: encPassword,
+    });
 
     //token
 
-    const token=jwt.sign(
-        {user_id:user._id,email     },
-        process.env.SECRET_KEY,
-        {
-            expiresIn:"2h"
-        }
-    )
-    user.token=token
+    const token = jwt.sign(
+      // creating token
+      { user_id: user._id, email },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    user.token = token;
     //upadte or not in DB
-    
-
 
     //TODO handel password situation
-user.password=undefined;       // so that password not shown up in the object which we are sending as json(user)
+    user.password = undefined; // so that password not shown up in the object which we are sending as json(user)
 
-    res.status(201).json(user)
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+app.post("/login", async (req, res) => {
+  //4 module 15 lesson of auth
+  try {
+    const { email, password } = req.body;
 
-
-
-       
-   }
-    catch (error) {
-       
-       console.log(error)
-
-   }
-})
-
-
-app.post("/login",async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        console.log(error);
+    if (!(email && password)) {
+      return res.status(400).send("field is missing");
+      console.log(ee);
     }
 
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      user.token = token;
+      user.password = undefined;
+      return res.status(200).json(user);
+    }
+    //   console.log("chala");
+    else
+
+    return res.status(400).send("Incorrect email or password");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/dashboard",auth, (req,res)=>{                //auth is a middleware
+    res.send("Welcome to dashboard")
 })
-module.exports=app; 
+module.exports = app;
---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const token =
+   ( req.header("Authorization").replace("Bearer", "") ||
+    req.cookies.token ||
+    req.body.token).trim();
+
+  if (!token) {
+    return res.status(403).send("token is missing ");
+  }
+ console.log();
+
+  try {
+    const decode = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(decode);
+    req.user=decode;
+    // bring info from DB
+    
+  } catch (error) {
+    return res.status(401).send("Invalid token");
+  }
+ return next();
+};
+
+module.exports = auth;
